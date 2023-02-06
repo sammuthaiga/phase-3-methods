b315795742e8e3c550249b78558f01bf062afe21
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-02-06 03:56:21 +0300

    Completed assignment

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ function greetProgrammer() {
   console.log("Hello, programmer!");
 }
 
+
 /*
   You should be able to call this function with one argument and see its output in the terminal:
   greet("Naureen");
@@ -38,6 +39,7 @@ function greetWithDefault(name = "programmer") {
 function add(num1, num2) {
   return num1 + num2;
 }
+add(1, 2);
 
 /*
   You should be able to call this function with two arguments and get back its return value:
@@ -54,3 +56,4 @@ function halve(number) {
 
   return number / 2;
 }
+halve(4)
--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+
+def add(num1, num2)
+    num1 + num2
+  end
+  
+  puts add(1, 2)
+
+def halve(number)
+    return nil unless number.is_a?(Numeric)
+    number / 2
+end
+  
+  puts halve(4)
